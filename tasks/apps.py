83ccc2e14de8b3c3e6d3e05ae0539e579df0fc1d
ibrahim barraza <barrazaibrahim@gmail.com>
2025-01-28 00:54:23 -0700

    user test2

--- tasks/apps.py
+++ tasks/apps.py
@@ -1,6 +1,20 @@
 from django.apps import AppConfig
+from django.db.utils import OperationalError
+
 
 
 class TasksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tasks'
+
+    def ready(self):
+            # Se ejecuta cuando Django carga esta app
+            try:
+                from django.contrib.auth.models import User
+                username = 'DrControl'
+                if not User.objects.filter(username=username).exists():
+                    User.objects.create_superuser(
+                        username, 'drcontrol@outlook.com', 'Mandarina12'
+                    )
+            except OperationalError:
+                pass
