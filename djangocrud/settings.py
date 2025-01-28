b43bb903ae822fab039206f193e062371f04fdcb
ibrahim barraza <barrazaibrahim@gmail.com>
2025-01-28 00:13:33 -0700

    Database test

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -79,15 +79,12 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-        'default' : dj_database_url.config(
-            default='postgresql://postgres:postgres@localhost/postgres',
-            conn_max_age=600
-        )
-    }
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost/postgres',
+        conn_max_age=600
+    )
 }
 
 
